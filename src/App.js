988b23ff9458aeba668ebdff743b9f6f74c2cb52
aryanmajeed <aryanmajid97@gmail.com>
2021-08-06 01:48:11 +0300

    Done.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0)
   useEffect(() => {
     console.log("useEffect called");
-  });
+  }, []);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return <button onClick={() => setCount(count + 1)}>Click Me</button>;
 }
 
 export default App;
